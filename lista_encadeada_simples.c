f4bc3696ba8b7b9e14dae4bbd4ce5d7d6ec1feee
Artur <asr.engsoft@gmail.com>
2025-05-22 14:17:31 -0300

    Otimização da lista encadeada simples

--- lista_encadeada_simples.c
+++ lista_encadeada_simples.c
@@ -1,81 +1,82 @@
 //Lista encadeada
 #include<stdio.h>
 #include<stdlib.h>
-#include<locale.h>
 
 //Criacao do tipo de variavel NO
 typedef struct NO{
     int valor;
-    int posicao;
     struct NO* prox;
 }NO;
 
-//Criacao da variavel de inicio para acessar o primeiro termo da lista e tamanho da lista
+//Criacao da variavel de inicio, tamanho
 NO* inicio=NULL;
 int tamanho=0;
 
 //Funcao para adicionar
 void add(int valor, int posicao){ 
-    //Verificar se a posicao é válida
     if((posicao>=0)&&(posicao<=tamanho)){
-        //Criaçao da variável temporaria "novo" para guardar os parametros da função na memoria com "malloc"
         NO* novo=malloc(sizeof(NO));
         novo->valor=valor;
-        novo->posicao=posicao;
+        novo->prox=NULL;
 
         //Casos de insercao:        
         if(inicio==NULL){ 
-            //Caso a lista esteja vazia
+            //lista vazia
             inicio = novo;
-            tamanho++;
         
         }else if(posicao==0){
-            //Caso a lista nao esteja vazia e eu quero add no inicio
+            //lista nao vazia e eu quero adicionar no inicio
             novo->prox=inicio;
             inicio=novo;
-            tamanho++;
 
         }else if(posicao==tamanho){
-            //Caso a lista nao esteja vazia e eu quero add no fim
+            //lista nao vazia e eu quero adicionar no final
             NO* aux=inicio;
             for(int i=0;i<tamanho-1;i++){
                 aux=aux->prox;
             }
             aux->prox=novo;
-            tamanho++;
         
         }else{
-            //Caso a lista nao esteja vazia e eu quero add em outra posicao
+            //lista nao vazia e eu quero adicionar em outra posicao
             NO* aux1=inicio;
             for(int i=0;i!=posicao-1;i++){
                 aux1=aux1->prox;
             }
             NO* aux2=aux1->prox;
             aux1->prox=novo;
-            novo->prox=aux2;
-            tamanho++;
+            novo->prox=aux2;    
         }
+    tamanho++;
+    }else{
+        printf("Insercao incorreta. Posicao invalida!\n");
     }
 }
 
 //Funcao para remover
 void remover(int posicao){
     if(posicao>=0&&posicao<tamanho){
+        NO* lixo=NULL;
+        
+        //Casos de remocao:
         if(posicao==0){
-            NO* aux=inicio;
-            inicio=aux->prox;
-            free(aux);
+            //remover do inicio
+            lixo=inicio;
+            inicio=lixo->prox;
+            
         }else{
+            //remover de outra posicao
             NO* aux1=inicio;
-            for(int i=0;i!=posicao-1;i++){
+            for(int i=0;i<posicao-1;i++){
                 aux1=aux1->prox;
             }
-            NO* aux2=aux1->prox;
-            aux1->prox=aux2->prox;
-            free(aux2);
-        }
+            lixo=aux1->prox;
+            aux1->prox=lixo->prox;
+        }        
+        free(lixo);
+        tamanho--;
     }else{
-        printf("Insercao incorreta. Posicao invalida!\n");
+        printf("Remocao incorreta. Posicao invalida!\n");
     }
 }
 
@@ -83,36 +84,32 @@ void remover(int posicao){
 void editar(int posicao, int valor){
     if(posicao>=0&&posicao<tamanho){
         NO* aux=inicio;
-        for(int i=0;i!=posicao;i++){
+        for(int i=0;i<posicao;i++){
             aux=aux->prox;
         }
         aux->valor=valor;
     }else{
-        printf("Insercao incorreta. Posicao invalida!\n");
+        printf("Edicao incorreta. Posicao invalida!\n");
     }
 }
 
 //Funcao para imprimir
 void imprimir(){
     if(tamanho>0){
-    }
-    printf("========== Valores do NO ==========\n");
+        printf("========== Valores do NO ==========\n");
         NO* aux=inicio;
-        for(int i=0;i!=tamanho;i++){
+        int i=0;
+        while(aux!=NULL){
             printf("O valor do NO na posicao %i: %i\n", i, aux->valor);
             aux=aux->prox;
+            i++;
+        }
+    }else{
+        printf("Nao foi possivel imprimir, lista vazia!\n");
     }
 }
 
+//Funcao principal
 int main(){
-    setlocale(LC_ALL, "Portuguese");
-
-    add(1,0);
-    add(2,1);
-    add(3,2);
-    add(4,3);
-    imprimir();
-    editar(1,4);
-    imprimir();
     return 0;
 }
